d1db19308eae9dd6dd70c0dab9170c95bd8fecff
Francesco Vallone <francesco.vallone@orbyta.it>
2024-10-30 12:58:46 +0100

    feat: update brick for project creation

--- bricks/base_application/__brick__/lib/app_controller.dart
+++ bricks/base_application/__brick__/lib/app_controller.dart
@@ -1,15 +1,39 @@
 import 'package:serinus/serinus.dart';
 
-import 'app_routes.dart';
+import 'todo.dart';
 
 class AppController extends Controller {
 
   AppController({super.path = '/'}){
-    on(HelloWorldRoute(), _handleHelloWorld);
+    on(Route.get('/'), _getAllTodos);
+    on(Route.get('/<index>'), _getTodo);
+    on(Route.put('/<index>'), _toggleTodo);
   }
 
-  Future<String> _handleHelloWorld(RequestContext context) async {
-    return 'Hello world';
+  Future<List<Todo>> _getAllTodos(RequestContext context) async {
+    return context.provider<AppProvider>().todos;
   }
+
+  Future<Todo> _getTodo(RequestContext context) async {
+    final index = int.tryParse(context.params['index'] ?? '');
+    if (index == null) {
+      throw BadRequestException('Invalid index');
+    }
+    final todos = context.provider<AppProvider>().todos;
+    if (todos.isEmpty || index < 0 || index >= todos.length) {
+      throw NotFoundException('Todo not found');
+    }
+    return todos[index];
+  }
+
+  Future<Todo> _toggleTodo(RequestContext context) async {
+    final index = int.tryParse(context.params['index'] ?? '');
+    if (index == null) {
+      throw BadRequestException('Invalid index');
+    }
+    context.provider<AppProvider>().toggleTodoStatus(index);
+    return context.provider<AppProvider>().todos[index];
+  }
+
 
 }
--- bricks/base_application/__brick__/lib/app_provider.dart
+++ bricks/base_application/__brick__/lib/app_provider.dart
@@ -1,11 +1,25 @@
 import 'package:serinus/serinus.dart';
 
+import 'todo.dart';
+
 class AppProvider extends Provider {
+
+  List<Todo> todos = [];
 
   AppProvider();
 
-  String sendHelloWorld() {
-    return 'Hello world!';
+  void addTodo(String name) {
+    todos.add(Todo(
+      title: name,
+    ));
+  }
+
+  void toggleTodoStatus(int index) {
+    todos[index].isDone = !todos[index].isDone;
+  }
+
+  void removeTodoAt(int index) {
+    todos.removeAt(index);
   }
 
 }
