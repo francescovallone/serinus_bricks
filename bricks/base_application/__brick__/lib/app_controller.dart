0858a5079c99edbc7f9304746417c5195b6a6efb
Francesco Vallone <francesco.vallone@orbyta.it>
2024-10-31 10:09:13 +0100

    fix: add create and delete route

--- bricks/base_application/__brick__/lib/app_controller.dart
+++ bricks/base_application/__brick__/lib/app_controller.dart
@@ -8,7 +8,9 @@ class AppController extends Controller {
   AppController({super.path = '/'}){
     on(Route.get('/'), _getAllTodos);
     on(Route.get('/<index>'), _getTodo);
+    on(Route.post('/') , _createTodo);
     on(Route.put('/<index>'), _toggleTodo);
+    on(Route.delete('/<index>'), _removeTodo);
   }
 
   Future<List<Todo>> _getAllTodos(RequestContext context) async {
@@ -34,6 +36,24 @@ class AppController extends Controller {
     }
     context.use<AppProvider>().toggleTodoStatus(index);
     return context.use<AppProvider>().todos[index];
+  }
+
+  Future<Todo> _createTodo(RequestContext context) async {
+    final body = context.body.json;
+    if (body == null || body['title'] == null) {
+      throw BadRequestException(message: 'Invalid request body');
+    }
+    final title = body['title'] as String;
+    context.use<AppProvider>().addTodo(title);
+    return context.use<AppProvider>().todos.last;
+  }
+
+  Future<void> _removeTodo(RequestContext context) async {
+    final index = int.tryParse(context.params['index'] ?? '');
+    if (index == null) {
+      throw BadRequestException(message: 'Invalid index');
+    }
+    context.use<AppProvider>().removeTodoAt(index);
   }
 
 
